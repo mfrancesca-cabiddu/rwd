62b1a2b6071a3cb880782452f820b03451b9d95c
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-03-30 13:41:01 +0200

    Update fixtures.js

--- js/fixtures.js
+++ js/fixtures.js
@@ -5,7 +5,7 @@ $(function () {
 
     vertigeText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
     vertige1 =  '<h1>Acrobatica Aerea</h1>' + vertigeText + '<img src="images/acrobatica aerea.jpg">' + vertigeText;
-    vertige2 =  '<h1>Kitesurf</h1>' + vertigeText + '<img src="images/kitesurf.jpg">' + vertigeText + vertigeText;
+    vertige2 =  '<h1>Kitesurf</h1>' + vertigeText + '<img src="images/kitesurf.jpg">' + vertigeText;
     vertige3 =  '<h1>Deltaplano</h1>' + vertigeText + '<img src="images/deltaplano.jpg">' + vertigeText;
     $(".vertige-text-1").html(vertige1);
     $(".vertige-text-2").html(vertige2);
